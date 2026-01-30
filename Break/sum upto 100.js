87a008b7d1fbb1059c460bf5f7ee8e6e6db7ad9a
mehedi hasan <mehedihasan.j135@gmail.com>
2026-01-30 21:21:24 +0600

    sum upto 100 task complete

---
+++ Break/sum upto 100.js
@@ -0,0 +1,16 @@
+/* 
+Write a while loop that adds numbers starting from 1, but stops (using break) as soon as the sum reaches or exceeds 100
+*/
+
+let i = 1;
+let sum = 0;
+while (true) {
+  sum = sum + i;
+  console.log(sum);
+  if (sum >= 100) {
+    break;
+  }
+
+  i++;
+}
+console.log("total sum = ", sum);
